9c250861d2c35575174d9a7a1c22bafa04714c11
THEKINGSTAR <thekingstar5002@gmail.com>
2024-03-21 16:58:10 +0200

    CORRECT ID NOT NULL IN THE SECRITP

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -8,7 +8,7 @@
 -- Context: Make an attribute unique directly in the table schema will enforced your business rules and avoid bugs in your application
 
 CREATE TABLE IF NOT EXISTS users (
-		id  INTEGER AUTO_INCREMENT PRIMARY KEY,
+		id  INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
 		email VARCHAR(255) NOT NULL UNIQUE,
 		name VARCHAR(255) NOT NULL
 		);
