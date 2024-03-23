83d0a87490584b5451507ccc9369ef9347fb50ce
THEKINGSTAR <thekingstar5002@gmail.com>
2024-03-24 01:31:23 +0200

    UPDATER THE TRIGER SYNTAX

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -3,17 +3,10 @@
 -- Context: Updating multiple tables for one action from your application
 -- can generate issue: network disconnection, crash, etc…
 -- to keep your data in a good shape, let MySQL do it for you!
-DELIMITER //
 CREATE TRIGGER Buy_buy_buy
 AFTER INSERT
 ON orders
 FOR EACH ROW
-BEGIN
-    UPDATE items
-    SET quantity = quantity - NEW.number
-        WHERE items.item_name = NEW.item_name;
-
-END;
-//
-DELIMITER ;
-
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
