f5cea8c7f225cf1f1aaf095106d20ab2bbe4a573
THEKINGSTAR <thekingstar5002@gmail.com>
2024-03-24 04:09:20 +0200

    5. Email validation to sent

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -2,10 +2,14 @@
 -- Requirements:
 -- Import this table dump: names.sql.zip
 -- Only the first letter of name AND score must be indexed
+DROP TRIGGER IF EXISTS valid_email;
+DELIMITER &&
 CREATE TRIGGER valid_email
-AFTER UPDATE
-ON users
-FOR EACH ROW
-    UPDATE users
-    SET valid_email = 0
-    WHERE id = NEW.id;
+    BEFORE UPDATE ON users
+    FOR EACH ROW
+    BEGIN
+        IF OLD.email <> NEW.email THEN
+            SET NEW.valid_email = 0;
+        END IF;
+    END &&
+DELIMITER ;
